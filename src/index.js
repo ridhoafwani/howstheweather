c8d4eee3632f2958d63e28a2ee11d0a1ee346cfe
ridhoafwani <zikrurridhoafwani@gmail.com>
2022-12-26 21:34:45 +0800

    Move from Gitlab to Github

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={"/howstheweather"}>
       <App />
     </Router>
   </React.StrictMode>,
